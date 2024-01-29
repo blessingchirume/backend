59f4a386aef0a67fc2275f53e4c5aa170cf1cdcb
blessing-chirume <chirume37@gmail.com>
2024-01-29 22:53:01 +0200

    updated seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Item;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -28,6 +29,23 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('12345678'),
         ]);
 
-        $this->call([ItemSeeder::class]);
+        $item = new Item();
+
+
+        $item->create([
+            'item_code' => rand(000000, 999999),
+            'item_description' => '1kg premium peaches',
+            'price' => '5.40',
+            'category' => 'Fruits and vegetables',
+            'image' => 'https://zonefresh.com.au/wp-content/uploads/PEACHES-WHITE--600x600.jpg'
+        ]);
+
+        $item->create([
+            'item_code' => rand(000000, 999999),
+            'item_description' => '6 pack standard grape ',
+            'price' => '5.40',
+            'category' => 'Fruits and vegetables',
+            'image' => 'https://www.westfaliafruit.com/wp-content/uploads/2019/01/Table-Grapes-300x300.jpg'
+        ]);
     }
 }
