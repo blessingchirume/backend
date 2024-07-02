444aaad8cec1f569cf18461946a0857a36437486
blessing-chirume <chirume37@gmail.com>
2024-07-03 00:18:41 +0200

    Renamed mismatching property item_no

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -224,14 +224,14 @@ class PaymentController extends Controller
                 $orderTotal = 0.0;
                 $itemIds = [];
 
-                // foreach ($request->orderDetails["order_items"] as $row) {
-                //     array_push($itemIds, $row['id']);
+                foreach ($request->orderDetails["order_items"] as $row) {
+                    array_push($itemIds, $row['id']);
 
-                //     $item = Item::where('item_code', $row['itemNo'])->first();
-                //     $orderTotal += ($item->price * $row['quantity']);
-                // }
+                    $item = Item::where('item_code', $row['item_no'])->first();
+                    $orderTotal += ($item->price * $row['quantity']);
+                }
 
-                return $request->orderDetails["order_items"];
+                // return $request->orderDetails["order_items"];
 
                 $orderId = DB::table('orders')->insertGetId([
                     'shipping_address' => $request->orderDetails["shipping_address"],
