be51f7054581a86d577a96503d9ba849fcaadc32
blessing-chirume <chirume37@gmail.com>
2024-07-21 17:00:16 +0200

    guest area

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return view('guest-area.index');
 });
 
 Route::get('/create', [UserController::class, 'create'])->name('user.create');
