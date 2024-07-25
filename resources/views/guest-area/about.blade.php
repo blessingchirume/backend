1626fc0fff42217008300c2e9e37b682a3a75976
blessing-chirume <chirume37@gmail.com>
2024-07-25 17:34:56 +0200

    refactored about page

--- resources/views/guest-area/about.blade.php
+++ resources/views/guest-area/about.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.guest')
 
 @section('content')
-<section class="hero-wrap hero-wrap-2" style="background-image: url({{ asset('lawncare/images/bg_2.jpg') }});" data-stellar-background-ratio="0.5">
+<section class="hero-wrap hero-wrap-2" style="background-image: url({{ asset('lawncare/images/apple-4.png') }});" data-stellar-background-ratio="0.5">
 	<div class="overlay"></div>
 	<div class="container">
 		<div class="row no-gutters slider-text align-items-end justify-content-center">
@@ -16,15 +16,15 @@
 <section class="ftco-section ftco-no-pt ftco-no-pb bg-light">
 	<div class="container">
 		<div class="row d-flex">
-			<div class="col-md-6 d-flex">
+			{{--<div class="col-md-6 d-flex">
 				<div class="img img-video d-flex align-self-stretch align-items-center justify-content-center justify-content-md-end" style="background-image: url({{ asset('lawncare/images/apple-5.jpg') }});">
 					<div class="overlay"></div>
 					<a href="https://vimeo.com/45830194" class="icon-video popup-vimeo d-flex justify-content-center align-items-center">
 						<span class="icon-play"></span>
 					</a>
 				</div>
-			</div>
-			<div class="col-md-6 pl-md-5">
+			</div>--}}
+			<div class="col-md-12 pl-md-5">
 				<div class="row justify-content-start py-5">
 					<div class="col-md-12 heading-section ftco-animate">
 						{{--<span class="subheading">Welcome to Lawn Care</span>--}}
