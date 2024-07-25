f2fb2358fb4a209d2c9622c858bd020f92f13139
blessing-chirume <chirume37@gmail.com>
2024-07-25 21:03:33 +0200

    meta data update

--- resources/views/includes/guest-area/head.blade.php
+++ resources/views/includes/guest-area/head.blade.php
@@ -1,6 +1,10 @@
 <head>
-    <title>Fresh Direct</title>
-    <meta charset="utf-8">
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Direct Fresh is a proudly Zimbabwean food delivery service provider for all ordinary Zimbabweans. Our products and services are engineered to give you a reliable, inclusive, safe, and convenient way to make purchases, payments, and receive your orders.">
+    <meta name="keywords" content="Direct Fresh, Zimbabwean food delivery, food delivery service, reliable food delivery, inclusive food delivery, safe food delivery, convenient food delivery, Zimbabwean food services, online food purchases, food delivery Zimbabwe">
+    <title>Direct Fresh - Zimbabwean Food Delivery Service</title>
+
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
